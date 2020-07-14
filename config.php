17f16c245f627b163e28b3c45d560b70b13d2872
MarcusDias-r <marcusdias22@hotmail.com>
2020-07-14 14:02:03 -0300

    first commit

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+require 'environment.php';
+
+$config = array();
+
+if(ENVIRONMENT == 'development'){
+    define("BASE_URL", "http://localhost/project");
+    $config['dbname'] = 'contaazul';
+    $config['host'] =   'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = '';
+}else{
+    define("BASE_URL", "http://localhost/project");
+    $config['dbname'] = 'contaazul';
+    $config['host'] =   'localhost';
+    $config['dbuser'] = 'root';
+    $config['dbpass'] = '';
+}
+
+global $db;
+try{
+
+    $pdo = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'],$config['dbuser'],$config['dbpass']);
+
+}catch(PDOException $e){
+
+    echo "ERRO: ".$e->getMessage();
+    exit;
+
+}
